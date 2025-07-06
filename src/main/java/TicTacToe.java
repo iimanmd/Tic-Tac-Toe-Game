71f3cddea72adf41e70e383e3b31789bc284e3f3
wi24b161 <wi24b161@technikum-wien.at>
2025-07-06 16:16:38 +0200

    US1: Grundgerüst der Klassen Player, Board und TicTacToe erstellt

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,18 @@
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+
+    public void start() {
+
+    }
+
+    public void switchCurrentPlayer() {
+
+    }
+
+    public boolean hasWinner() {
+        return false; // vorerst false
+    }
+}
